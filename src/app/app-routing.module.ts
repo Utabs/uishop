3a8ed9e282fce1ba96089fea532959a721a86283
utab <aminrafiei1985@gmail.com>
2023-02-07 10:26:04 +0330

    --alter shpui file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BranchComponent} from "./modules/branch/branch.component";
 
 const routes: Routes = [
-  { path:'branch',loadChildren : ()=>import('./modules/branch/branch.module').then(m=>m.BranchModule)}
+  // { path: 'branch',component: BranchComponent}
+  { path: 'branch',   loadChildren: () => import('./modules/branch/branch.module').then(m=>m.BranchModule)},
+  { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) },
+  { path: 'basket', loadChildren: () => import('./modules/purchase/purchase.module').then(m => m.PurchaseModule) },
+  { path: 'customer', loadChildren: () => import('./modules/customer/customer.module').then(m => m.CustomerModule) }
 ];
 
 @NgModule({
